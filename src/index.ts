83c6cbf8710ab4b63952c1844e45c46a6291eab7
synicalmx <70176240+SynicalMX@users.noreply.github.com>
2024-11-01 19:11:41 -0400

    fix(jsr): not finding types when publishing

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import ScriptBuilder from "./script";
 import { ScratchStage, ScratchSprite } from "./object";
 import { Opcode } from "./opcodes";
 import { ScratchType } from "./scratch_type";
-import type { ScratchCompiler } from "./types";
+import type { ScratchCompiler } from "./types.d";
 
 /**
  * ScratchCompiler module
  */
 const module: ScratchCompiler = {
-	ProjectBuilder: ProjectBuilder,
+	ProjectBuilder: ProjectBuilder,:
 	ScriptBuilder: ScriptBuilder,
 	ScratchSprite: ScratchSprite,
 	ScratchStage: ScratchStage,
